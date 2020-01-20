39d47db5db5ee1d2d31cba9d9f03baaf25ba351c
cuiran <cuiran2001@163.com>
2020-01-20 08:52:28 +0800

    @log 添加测试类

--- src/main/java/com/cayden/ethereum/demo/TestVoting.java
+++ src/main/java/com/cayden/ethereum/demo/TestVoting.java
@@ -7,6 +7,8 @@ import org.web3j.crypto.Wallet;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.RemoteCall;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.tx.Contract;
 import org.web3j.tx.gas.ContractGasProvider;
 
 import java.io.File;
@@ -27,39 +29,25 @@ public class TestVoting {
     public static void main(String [] args){
         try{
             Web3j web3j= Web3JClient.getClient();
+            Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
+            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+
+            System.out.println("clientVersion:"+ clientVersion);
             //为用户创建凭证credentials
             String filePath = "/Users/cuiran/ethereum/web3j/wallet";
-            String fileName = WalletUtils.generateNewWalletFile("123456",new File(filePath),false);
+            File file=new File(filePath);
+            System.out.println("file:"+ file.isDirectory());
+            String fileName = WalletUtils.generateLightNewWalletFile("a12345678",new File(filePath));
 
             System.out.println("file:"+filePath + "/" + fileName);
-            Credentials credentials = WalletUtils.loadCredentials("123456", filePath + "/" + fileName);
+            Credentials credentials = WalletUtils.loadCredentials("a12345678", filePath + "/" + fileName);
+
             List<byte[]> list=new ArrayList<>();
             list.add("zhangsan".getBytes());
             list.add("lisi".getBytes());
 
-            ContractGasProvider contractGasProvider=new ContractGasProvider() {
-                @Override
-                public BigInteger getGasPrice(String s) {
-                    return BigInteger.valueOf(100);
-                }
-
-                @Override
-                public BigInteger getGasPrice() {
-                    return BigInteger.valueOf(100);
-                }
-
-                @Override
-                public BigInteger getGasLimit(String s) {
-                    return BigInteger.valueOf(100);
-                }
-
-                @Override
-                public BigInteger getGasLimit() {
-                    return BigInteger.valueOf(100);
-                }
-            };
             //这样就可以创建一个凭证
-            Voting  voting=Voting.deploy(web3j,credentials,contractGasProvider,list).send();
+            Voting  voting=Voting.deploy(web3j,credentials,Contract.GAS_PRICE, Contract.GAS_LIMIT,list).send();
             System.out.println(voting.getContractAddress());
         }catch (Exception e){
             e.printStackTrace();
